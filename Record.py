913bf1f6da790306a1a8661edb6a8a1157335d3c
Fanil Nazirov <102851657+FanatiooN@users.noreply.github.com>
2024-01-02 00:23:42 +0300

    feat(Record.py): add pydantic validation

--- Record.py
+++ Record.py
@@ -1,5 +1,6 @@
 from datetime import date, time
 from enum import Enum
+from pydantic import BaseModel
 
 
 class Duration(Enum):
@@ -16,7 +17,26 @@ class Difficulty(Enum):
     NOT_SELECTED = "Не выбрано"
 
 
-class Record:
+from datetime import date, time
+from enum import Enum
+from pydantic import BaseModel
+
+
+class Duration(Enum):
+    THIRTY_MINUTES = "30 минут"
+    ONE_HOUR = "1 час"
+    GROUP_CALL = "Групповой созвон"
+    NOT_SELECTED = "Не выбрано"
+
+
+class Difficulty(Enum):
+    LIGHT = "Лайт"
+    STANDARD = "Стандарт"
+    HARD = "Хард"
+    NOT_SELECTED = "Не выбрано"
+
+
+class Record(BaseModel):
     date: date
     time: time
     fullname: str
