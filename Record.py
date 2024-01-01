b491af710ed87291ef95fd153ca6e11b755dba11
Fanil Nazirov <102851657+FanatiooN@users.noreply.github.com>
2024-01-02 00:48:18 +0300

    fix(Record.py): fix classes

--- Record.py
+++ Record.py
@@ -17,25 +17,6 @@ class Difficulty(Enum):
     NOT_SELECTED = "Не выбрано"
 
 
-from datetime import date, time
-from enum import Enum
-from pydantic import BaseModel
-
-
-class Duration(Enum):
-    THIRTY_MINUTES = "30 минут"
-    ONE_HOUR = "1 час"
-    GROUP_CALL = "Групповой созвон"
-    NOT_SELECTED = "Не выбрано"
-
-
-class Difficulty(Enum):
-    LIGHT = "Лайт"
-    STANDARD = "Стандарт"
-    HARD = "Хард"
-    NOT_SELECTED = "Не выбрано"
-
-
 class Record(BaseModel):
     date: date
     time: time
